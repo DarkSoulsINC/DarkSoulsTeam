0c3f13c378c03425bd761462024d29fcbe3eda53
DarkSoulsINC <nicopronext@gmail.com>
2024-05-25 01:21:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('Il sito è stato caricato con successo!');
+    console.log('Il sito è stato caricato con successo!');
+
+    const sections = document.querySelectorAll('.section');
+
+    const options = {
+        root: null,
+        threshold: 0.1,
+        rootMargin: '0px'
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeInUp');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
 });
